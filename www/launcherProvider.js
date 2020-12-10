b4bdc59ea1b955295f44b37d4565202ccd84b21a
Alan Lampa <alan.lampa@bluefletch.com>
2020-12-09 19:34:14 -0500

    Fix LauncherProvider already declared error

--- www/launcherProvider.js
+++ www/launcherProvider.js
@@ -4,27 +4,27 @@ const exec = require('cordova/exec');
  * Constructor
  * @returns {LauncherProvider}
  */
-function LauncherProvider() {}
+function LauncherProviderPlugin() {}
 
-LauncherProvider.prototype.getCurrentSessionAsJson = function(callback) {
+LauncherProviderPlugin.prototype.getCurrentSessionAsJson = function(callback) {
     exec(callback, null, 'LauncherProviderPlugin', 'getCurrentSessionAsJson', []);
 }
 
-LauncherProvider.prototype.addConfigExtAttribute = function (key, value) {
+LauncherProviderPlugin.prototype.addConfigExtAttribute = function (key, value) {
     exec(null, null, 'LauncherProviderPlugin', 'addConfigExtAttribute', [key, value])
 }
 
-LauncherProvider.prototype.removeConfigExtAttribute = function (key, value) {
+LauncherProviderPlugin.prototype.removeConfigExtAttribute = function (key, value) {
     exec(null, null, 'LauncherProviderPlugin', 'removeConfigExtAttribute', [key, value])
 }
 
-LauncherProvider.prototype.addSessionExtAttribute = function (key, value) {
+LauncherProviderPlugin.prototype.addSessionExtAttribute = function (key, value) {
     exec(null, null, 'LauncherProviderPlugin', 'addSessionExtAttribute', [key, value])
 }
 
-LauncherProvider.prototype.removeSessionExtAttribute = function (key, value) {
+LauncherProviderPlugin.prototype.removeSessionExtAttribute = function (key, value) {
     exec(null, null, 'LauncherProviderPlugin', 'removeSessionExtAttribute', [key, value])
 }
 
-const LauncherProvider = new LauncherProvider();
+const LauncherProvider = new LauncherProviderPlugin();
 module.exports = LauncherProvider;
